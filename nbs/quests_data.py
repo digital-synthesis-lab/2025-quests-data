cbbebef34837f3fd458d3ac9f852e850235d8c14
Daniel Schwalbe-Koda <danielskoda@gmail.com>
2025-03-12 20:59:11 -0700

    python script to import a few useful packages

---
+++ nbs/quests_data.py
@@ -0,0 +1,13 @@
+import os
+import re
+import sys
+import json
+import tqdm
+import numpy as np
+import pandas as pd
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+plt.style.use("paper")
